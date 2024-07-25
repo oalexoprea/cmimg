35b2305a9f20ecca724a5e8b33bf5de91fcc5655
Alexandru Oprea <alexandruoprea@Alexandrus-MacBook-Air.local>
2024-07-25 11:47:24 +0300

    Add support for mov files

--- cmimg.py
+++ cmimg.py
@@ -2,6 +2,8 @@ import os
 from datetime import datetime
 import PIL.Image
 import PIL.ExifTags
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
 
 def get_file_meta(file):
     exif = {}
@@ -20,12 +22,21 @@ dir_list = os.listdir(".")
 inc = 0
 for i in dir_list:
     inc+= 1
+    file_ext = os.path.splitext(i)[1]
+    if file_ext.lower() == ".mov":
+        parser = createParser(i)
+        metadata = extractMetadata(parser)
+        datetime_object = metadata.get('creation_date')
+        fmt_date = "%Y:%m:%d %H:%M:%S"
+        new_name = datetime_object.strftime('%Y%m%d_%H%M%S') + str(inc) + "i" + file_ext
+        print("Rename "+i + " to " + new_name)
+        os.rename(i,new_name)
+
     if "DateTime" in get_file_meta(i).keys():
         str_date = get_file_meta(i)["DateTime"][:-1]
         fmt_date = "%Y:%m:%d %H:%M:%S"
         datetime_object = datetime.strptime(str_date, fmt_date )
         make = str(get_file_meta(i)["Make"])
-        file_ext = os.path.splitext(i)[1]
         if make.strip().lower() == "apple":
             new_name = datetime_object.strftime('%Y%m%d_%H%M%S') + str(inc) + "i" + file_ext
             print("Rename "+i + " to " + new_name)
